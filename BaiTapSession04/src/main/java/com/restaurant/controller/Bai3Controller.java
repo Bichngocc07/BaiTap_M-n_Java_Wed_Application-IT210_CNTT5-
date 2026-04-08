773a66391928993997c3e8cf3125a886a4af0bfc
Bichngocc07 <ngoc01072006@gmail.com>
2026-04-08 20:46:04 +0700

    Add files via upload

--- BaiTapSession04/src/main/java/com/restaurant/controller/Bai3Controller.java
+++ BaiTapSession04/src/main/java/com/restaurant/controller/Bai3Controller.java
@@ -7,14 +7,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/bai3")
 public class Bai3Controller {
 
-    // Cách A (chuẩn REST)
+    
     @GetMapping("/orders/{id}")
     @ResponseBody
     public String getOrderByPath(@PathVariable long id) {
         return "Chi tiết đơn hàng số " + id;
     }
 
-    // Cách B (query string)
     @GetMapping("/orders")
     @ResponseBody
     public String getOrderByParam(@RequestParam long id) {
